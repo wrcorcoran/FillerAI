45acb24297343d245cb611a9a8f29fa3662b49c0
wcorcoran2026 <wcorcoran@ucsb.edu>
2023-12-19 16:10:23 -0800

    adding backend

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     basePath: "",
     images: {
         unoptimized: true,
     },
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://127.0.0.1:5328/:path*", // Proxy to Backend
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: "/api/:path*",
+    //             destination: "http://127.0.0.1:5328/:path*", // Proxy to Backend
+    //         },
+    //     ];
+    // },
 };
 
 module.exports = nextConfig;
