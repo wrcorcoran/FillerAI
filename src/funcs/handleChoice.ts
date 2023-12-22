2ea4564dc0b408ae863d4c9865ebd4a0a1a60673
wcorcoran2026 <wcorcoran@ucsb.edu>
2023-12-21 18:19:01 -0800

    adding complete minimax functionalty

--- src/funcs/handleChoice.ts
+++ src/funcs/handleChoice.ts
@@ -14,7 +14,7 @@ export default async function handleBotChoice(
 
         await minimax(
             board.cloneBoard(),
-            11,
+            9,
             -Infinity,
             +Infinity,
             true,
